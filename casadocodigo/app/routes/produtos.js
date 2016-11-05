b9dfe784cc6cf804cf02b3ffb378da1a4517ed00
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-11-05 18:14:57 -0200

    Mysql connect and list books

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,20 @@
 
 module.exports = function( app ) {
   app.get( '/produtos', function( request, response ) {
-    response.render( 'produtos/lista' );
+
+    var mysql = require( 'mysql' );
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'casadocodigo'
+    });
+
+    connection.query( 'select * from livros', function( error, result ) {
+      response.send( result );
+    });
+
+    connection.end();
+
   });
 };
