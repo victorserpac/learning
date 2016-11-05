d515da7155ac1707d547a90259ad0da99d7badca
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-11-05 16:15:54 -0200

    Using EJS to render the views

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,8 +1,11 @@
 var express = require( 'express' );
 var app = express();
 
+app.set( 'view engine', 'ejs' );
+
 app.get( '/produtos', function( request, response ) {
-  response.send( '<html><body><h1>Listando os produtos</h1></body></html>' );
+  console.log( 'Listando' );
+  response.render( 'produtos/lista' );
 });
 
 app.listen( 3000, function(){
