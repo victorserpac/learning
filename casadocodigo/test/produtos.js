ef3aefdfc6f6b6e33b83a8130ef4edc9d0897c31
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-08 11:28:52 -0200

    Implement Mocha simple test

---
+++ casadocodigo/test/produtos.js
@@ -0,0 +1,15 @@
+var http = require('http');
+describe( '#ProdutosController', function() {
+  it( '#listagem json', function() {
+    console.log('teste de verificação de listagem json ');
+  });
+});
+
+
+
+// produtoController
+  // lista json
+  //
+  // cadastro aceita json
+  //
+  // cadastro aceita urlencoded
