54ac43928cddaeb68c1383af1155675b4ff0a707
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-08 11:54:22 -0200

    Test product registration

--- casadocodigo/test/produtos.js
+++ casadocodigo/test/produtos.js
@@ -3,11 +3,22 @@ var request = require( 'supertest' )( express );
 
 describe( '#ProdutosController', function() {
   it( '#listagem json', function( done ) {
-
     request.get( '/produtos' )
       .set( 'Accept', 'application/json' )
       .expect( 'Content-Type', /json/ )
       .expect( 200, done );
+  });
+
+  it( '#cadastro de novo produto com dados invalidos', function( done ) {
+    request.post( '/produtos' )
+      .send( { titulo:'', descricao:'novo livro' } )
+      .expect( 400, done );
+  });
+
+  it( '#cadastro de novo produto com dados validos', function( done ) {
+    request.post( '/produtos' )
+      .send( { titulo:'titulo', descricao:'novo livro', preco: 20.50 } )
+      .expect( 302, done );
   });
 });
 
