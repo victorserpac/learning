c801159bb9ba28ad69adfea2ec3727e25f04936a
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-08 11:31:07 -0200

    Test productsConstroller http response

--- casadocodigo/test/produtos.js
+++ casadocodigo/test/produtos.js
@@ -1,7 +1,25 @@
 var http = require('http');
+
 describe( '#ProdutosController', function() {
-  it( '#listagem json', function() {
-    console.log('teste de verificação de listagem json ');
+  it( '#listagem json', function( done ) {
+    var configuracoes = {
+      hostname: 'localhost',
+      port: 3000,
+      path: '/produtos',
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+
+    http.get( configuracoes, function( res ) {
+      if ( res.statusCode == 200 ) {
+        console.log( 'Status ta OK' );
+      }
+      if ( res.headers[ 'content-type' ] == 'application/json; charset=utf-8' ) {
+        console.log( 'Content Type ok' );
+      }
+      done();
+    });
   });
 });
 
