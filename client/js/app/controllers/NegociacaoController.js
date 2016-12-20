c36929e07ebc3458d2e230dbcad3ac73f8029203
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-20 21:09:12 -0200

    Some ways to create Date object

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,17 +3,33 @@ class NegociacaoController {
   constructor() {
     let $ = document.querySelector.bind( document ); // Keep association with document
 
-    this.inputData       = $( '#data' );
-    this.inputQuantidade = $( '#quantidade' );
-    this.inputValor      = $( '#valor' );
+    this._inputData       = $( '#data' );
+    this._inputQuantidade = $( '#quantidade' );
+    this._inputValor      = $( '#valor' );
+
   }
 
   add( event ) {
     event.preventDefault();
 
-    console.log( this.inputData.value );
-    console.log( this.inputQuantidade.value );
-    console.log( this.inputValor.value );
+
+    // '2016,11,12'
+    // let data = new Date( this._inputData.value.replace(/-/g, ',') );
+
+    // ['2016', '11', '12']
+    // [2016, 11, 12]
+    let data = new Date( this._inputData.value.split( '-' ) );
+
+    console.log( data );
+
+    // let negociacao = new Negociacao(
+    //   this._inputData.value,
+    //   this._inputQuantidade.value,
+    //   this._inputValor.value
+    // );
+
+    // console.log( negociacao );
+    // adicionar a negociacao em uma lista
   }
 
 }
