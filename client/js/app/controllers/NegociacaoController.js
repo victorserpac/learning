70409ae6009a0f609476133746e956be65272f0e
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-21 16:10:16 -0200

    Create a trigger to update the view as model is updated

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,10 +6,13 @@ class NegociacaoController {
     this._inputData        = $( '#data' );
     this._inputQuantidade  = $( '#quantidade' );
     this._inputValor       = $( '#valor' );
-    this._listaNegociacoes = new ListaNegociacoes();
+    this._listaNegociacoes = new ListaNegociacoes( this, function( model ) {
+      this._negociacoesView.update( model );
+    });
 
     this._negociacoesView  = new NegociacoesView( $( '#negociacoesView' ) );
     this._negociacoesView.update( this._listaNegociacoes );
+
     this._mensagem = new Mensagem();
     this._mensagemView = new MensagemView( $( '#mensagemView' ) );
     this._mensagemView.update( this._mensagem );
@@ -19,7 +22,6 @@ class NegociacaoController {
     event.preventDefault();
 
     this._listaNegociacoes.adiciona( this._criaNegociacao() );
-    this._negociacoesView.update( this._listaNegociacoes );
 
     this._mensagem.texto = 'Negociação adicionada com sucesso';
     this._mensagemView.update( this._mensagem );
@@ -29,7 +31,6 @@ class NegociacaoController {
 
   apaga() {
     this._listaNegociacoes.esvazia();
-    this._negociacoesView.update( this._listaNegociacoes );
 
     this._mensagem.texto = 'Negociações apagadas com sucesso';
     this._mensagemView.update( this._mensagem );
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,11 +1,15 @@
 class ListaNegociacoes {
 
-  constructor() {
+  constructor( contexto, armadilha ) {
     this._negociacoes = [];
+    this._armadilha = armadilha;
+    this._contexto = contexto;
   }
 
   adiciona( negociacao ) {
     this._negociacoes.push( negociacao );
+    // this._armadilha( this );
+    Reflect.apply( this._armadilha, this._contexto, [ this ] );
   }
 
   get negociacoes() {
@@ -14,5 +18,7 @@ class ListaNegociacoes {
 
   esvazia() {
     this._negociacoes = [];
+    // this._armadilha( this );
+    Reflect.apply( this._armadilha, this._contexto, [ this ] );
   }
 }
