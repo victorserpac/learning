4b886ff1036feedd45b04e01fd16e9992c2d7dd2
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-21 09:21:08 -0200

    Create negotiation list model

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,25 +6,33 @@ class NegociacaoController {
     this._inputData       = $( '#data' );
     this._inputQuantidade = $( '#quantidade' );
     this._inputValor      = $( '#valor' );
-
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   add( event ) {
     event.preventDefault();
 
-    // Spread operator
+    this._listaNegociacoes.adiciona( this._criaNegociacao() );
+    this._limpaFormulario();
 
-    let negociacao = new Negociacao(
+    console.log( this._listaNegociacoes.negociacoes );
+
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
       DateHelper.textoParaData( this._inputData.value ),
       this._inputQuantidade.value,
       this._inputValor.value
     );
+  }
 
-    console.log( negociacao );
+  _limpaFormulario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
 
-
-    console.log( DateHelper.dataParaTexto( negociacao.date ) );
-    // adicionar a negociacao em uma lista
+    this._inputData.focus();
   }
 
 }
