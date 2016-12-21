49d095226f1869072c8af31c40cbcfaf505bb2d2
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-21 11:50:31 -0200

    Implement class inheritance in ES6

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
   constructor( elemento ) {
-    this._elemento = elemento;
+    super( elemento );
   }
 
   _template( model ) {
@@ -37,9 +37,5 @@ class NegociacoesView {
         </tfoot>
       </table>
     `;
-  }
-
-  update( model ) {
-    this._elemento.innerHTML = this._template( model );
   }
 }
