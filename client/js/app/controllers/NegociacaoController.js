b113306c8aa38f1115c60db500c8a36705cad55e
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-22 10:36:13 -0200

    Implement import negotiations with ajax

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -30,6 +30,32 @@ class NegociacaoController {
     this._mensagem.texto = 'Negociações apagadas com sucesso';
   }
 
+  importaNegociacoes() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'negociacoes/semana' );
+
+    xhr.onreadystatechange = () => {
+      if( xhr.readyState == 4 ) {
+        if( xhr.status == 200 ) {
+
+          JSON.parse( xhr.responseText )
+            .map( objeto => new Negociacao( new Date( objeto.data ), objeto.quantidade, objeto.valor ) )
+            .forEach( negociacao => this._listaNegociacoes.adiciona( negociacao ) );
+
+          this._mensagem.texto = 'Negociacoes importadas com sucesso';
+        } else {
+          console.log( xhr.responseText );
+          this._mensagem.texto = 'Não foi possível obeter as negociacoes do servidor';
+        }
+      }
+    };
+
+    xhr.send();
+
+
+  }
+
   _criaNegociacao() {
     return new Negociacao(
       DateHelper.textoParaData( this._inputData.value ),
