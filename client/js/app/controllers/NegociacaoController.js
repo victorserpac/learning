dedd3e0e85a611d025c9c4175c0be51fd4e8f329
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-22 10:46:30 -0200

    Improve the ajax request implementation

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -31,29 +31,18 @@ class NegociacaoController {
   }
 
   importaNegociacoes() {
-    let xhr = new XMLHttpRequest();
+    let service = new NegociacaoService();
 
-    xhr.open('GET', 'negociacoes/semana' );
-
-    xhr.onreadystatechange = () => {
-      if( xhr.readyState == 4 ) {
-        if( xhr.status == 200 ) {
-
-          JSON.parse( xhr.responseText )
-            .map( objeto => new Negociacao( new Date( objeto.data ), objeto.quantidade, objeto.valor ) )
-            .forEach( negociacao => this._listaNegociacoes.adiciona( negociacao ) );
-
-          this._mensagem.texto = 'Negociacoes importadas com sucesso';
-        } else {
-          console.log( xhr.responseText );
-          this._mensagem.texto = 'Não foi possível obeter as negociacoes do servidor';
-        }
+    service.obterNegociacoesDaSemana( ( err, negociacoes ) => {
+      if ( err ) {
+        this._mensagem.texto = err;
+        return;
       }
-    };
 
-    xhr.send();
+      negociacoes.forEach( negociacao => this._listaNegociacoes.adiciona( negociacao ) );
+      this._mensagem.texto = 'Negociações importadas com sucesso';
 
-
+    });
   }
 
   _criaNegociacao() {
