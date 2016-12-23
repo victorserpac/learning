120a4c375eefc4d609e37253f7f3d57a942e4a87
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-23 15:45:57 -0200

    Use NegociacaoDao in controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -20,10 +20,20 @@ class NegociacaoController {
   adiciona( event ) {
     event.preventDefault();
 
-    this._listaNegociacoes.adiciona( this._criaNegociacao() );
-    this._mensagem.texto = 'Negociação adicionada com sucesso';
+    ConnectionFactory
+      .getConnection()
+      .then( connection => {
+        let negociacao = this._criaNegociacao();
 
-    this._limpaFormulario();
+        new NegociacaoDao( connection )
+          .adiciona( negociacao )
+          .then( () => {
+            this._listaNegociacoes.adiciona( negociacao );
+            this._mensagem.texto = 'Negociação adicionada com sucesso';
+            this._limpaFormulario();
+          });
+      })
+      .catch( erro => this._mensagem.texto = erro );
   }
 
   apaga() {
@@ -55,8 +65,8 @@ class NegociacaoController {
   _criaNegociacao() {
     return new Negociacao(
       DateHelper.textoParaData( this._inputData.value ),
-      this._inputQuantidade.value,
-      this._inputValor.value
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
     );
   }
 
