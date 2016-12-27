a41417f60ecfe0f662c1e38197002548d552e1e2
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-27 21:46:04 -0200

    Create a directive wich get data from server

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,4 +1,7 @@
-angular.module( 'minhasDiretivas', [] )
+angular
+  .module( 'minhasDiretivas', [
+    'meusServicos'
+  ])
   .directive( 'meuPainel', function() {
 
     var ddo = {};
@@ -62,5 +65,19 @@ angular.module( 'minhasDiretivas', [] )
       });
     };
 
+    return ddo;
+  })
+
+  .directive( 'meusTitulos', function(){
+    var ddo = {};
+    ddo.restrict = 'E';
+    ddo.template = '<ul><li ng-repeat="titulo in titulos">{{ titulo }}</li></ul>';
+    ddo.controller = function( $scope, recursoFoto ) {
+      recursoFoto.query( function( fotos ) {
+        $scope.titulos = fotos.map( function( foto ){
+          return foto.titulo;
+        });
+      });
+    };
     return ddo;
   });
