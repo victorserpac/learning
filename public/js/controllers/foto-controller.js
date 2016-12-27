8b02e6ba1f6f303177242e5a67fc3237d330cedd
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-27 15:10:11 -0200

    Implement picture creation

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,20 @@
-angular.module( 'alurapic' ).controller('FotoController', function($scope){
+angular.module( 'alurapic' ).controller( 'FotoController', function( $scope, $http ) {
 
   $scope.foto = {};
+  $scope.mensagem = '';
 
   $scope.submeter = function() {
-    console.log( $scope.foto );
+    if ( $scope.formulario.$valid ) {
+      $http.post( 'v1/fotos', $scope.foto )
+        .success( function() {
+          $scope.foto = {};
+          $scope.mensagem = 'Foto incluida com sucesso';
+        })
+        .error( function( error ){
+          $scope.mensagem = 'Não foi possível incluir a foto';
+          console.log( error );
+        });
+    }
   };
 
 });
