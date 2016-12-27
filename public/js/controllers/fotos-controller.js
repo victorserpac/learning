59f608819da9409337082b2556f451bbad700d0b
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-26 23:17:13 -0200

    First steps, create a controller to associate with scope

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module( 'alurapic' ).controller( 'FotosController', function( $scope ) {
+
+  $scope.foto = {
+    titulo: 'Gato',
+    url: 'http://elelur.com/data_images/mammals/cat/cat-01.jpg'
+  };
+
+});
