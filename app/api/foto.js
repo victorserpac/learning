3bfe0a4b5402e6335656be85508dc2948532d1c5
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-29 17:15:37 -0200

    Implement list API picture with MongoDB

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,57 +1,36 @@
+var mongoose = require( 'mongoose' );
 var api = {};
-var contador = 2;
-
-var fotos = [
-  { _id: 1, titulo: 'Leão', url: 'asd' },
-  { _id: 2, titulo: 'Leão 2', url: 'asdf' }
-];
 
 api.lista = function( req, res ) {
 
-  res.json( fotos );
-
+  var model = mongoose.model( 'Foto' );
+  model
+    .find({})
+    .then( function( fotos ){
+      res.json( fotos );
+    })
+    .catch( function( error ){
+      console.log( error );
+      res.status( 500 ).json( error );
+    });
 };
 
 
 api.buscaPorId = function( req, res ) {
 
-
-  var foto = fotos.find( foto => {
-    return foto._id == req.params.id;
-  });
-
-  res.json( foto );
 };
 
 
 api.removePorId = function( req, res ){
 
-  fotos = fotos.filter( foto => {
-    return foto._id != req.params.id;
-  });
-
-  res.sendStatus( 204 );
 };
 
 api.adiciona = function( req, res ) {
-  var foto = req.body;
-  foto._id = ++contador;
-  fotos.push( foto );
 
-  res.json( foto );
 };
 
 api.atualiza = function( req, res ) {
-  var foto = req.body;
-  var fotoId = req.params.id;
 
-  var indice = fotos.findIndex( foto => {
-    return foto._id == fotoId;
-  });
-
-  fotos[ indice ] = foto;
-
-  res.sendStatus( 200 );
 };
 
 
