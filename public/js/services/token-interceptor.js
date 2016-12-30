ee41fc846e34802c06f687219cef8f4246b2b45e
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-30 17:16:09 -0200

    Send to login if there is no token

--- public/js/services/token-interceptor.js
+++ public/js/services/token-interceptor.js
@@ -1,5 +1,5 @@
 angular.module( 'alurapic' )
-  .factory( 'tokenInterceptor', function( $window ) {
+  .factory( 'tokenInterceptor', function( $window, $q, $location ) {
     var interceptor = {};
 
     interceptor.response = function( response ) {
@@ -20,6 +20,16 @@ angular.module( 'alurapic' )
       }
 
       return config;
+    };
+
+    interceptor.responseError = function( rejection ) {
+      if ( rejection !== null && rejection.status == 401 ) {
+        delete $window.sessionStorage.token;
+        $location.path( '/login' );
+      }
+
+      return $q.reject( rejection );
+
     };
 
     return interceptor;
