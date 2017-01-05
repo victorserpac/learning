8a476ff2bd8bef7f6ba351578fa167e2aa282028
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-05 11:07:56 -0200

    Running application with cluster

---
+++ payfast/cluster.js
@@ -0,0 +1,21 @@
+var cluster = require( 'cluster' );
+var os      = require( 'os' );
+
+
+var cpus = os.cpus();
+
+// console.log( cpus );
+
+console.log( 'executando thread' );
+
+if ( cluster.isMaster ) {
+  console.log( 'thread master' );
+
+  cpus.forEach( function() {
+    cluster.fork();
+  });
+
+} else {
+  console.log( 'thread slave' );
+  require( './index' );
+}
