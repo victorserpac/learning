ee97687cf139ab43fae37cd2e202f1dc34b40b0e
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-09 23:17:25 -0200

    Use ngModel to simplify

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -8,16 +8,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+require("rxjs/add/operator/map");
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
-var foto_module_1 = require("./foto/foto.module");
-var http_1 = require("@angular/http");
-require("rxjs/add/operator/map");
-var painel_module_1 = require("./painel/painel.module");
-var cadastro_component_1 = require("./cadastro/cadastro.component");
 var listagem_component_1 = require("./listagem/listagem.component");
+var cadastro_component_1 = require("./cadastro/cadastro.component");
+var http_1 = require("@angular/http");
+var foto_module_1 = require("./foto/foto.module");
+var painel_module_1 = require("./painel/painel.module");
 var app_routes_1 = require("./app.routes");
+var forms_1 = require("@angular/forms");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -25,8 +26,15 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule, foto_module_1.FotoModule, http_1.HttpModule, painel_module_1.PainelModule, app_routes_1.routing],
-        declarations: [app_component_1.AppComponent, cadastro_component_1.CadastroComponent, listagem_component_1.ListagemComponent],
+        imports: [
+            platform_browser_1.BrowserModule,
+            http_1.HttpModule,
+            painel_module_1.PainelModule,
+            foto_module_1.FotoModule,
+            app_routes_1.routing,
+            forms_1.FormsModule
+        ],
+        declarations: [app_component_1.AppComponent, listagem_component_1.ListagemComponent, cadastro_component_1.CadastroComponent],
         bootstrap: [app_component_1.AppComponent]
     }),
     __metadata("design:paramtypes", [])
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule }  from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { FotoModule } from './foto/foto.module';
-import { HttpModule } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { ListagemComponent }   from './listagem/listagem.component';
+import { CadastroComponent }   from './cadastro/cadastro.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FotoModule } from './foto/foto.module';
 import { PainelModule } from './painel/painel.module';
-import { CadastroComponent } from './cadastro/cadastro.component';
-import { ListagemComponent } from './listagem/listagem.component';
 import { routing } from './app.routes';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [ BrowserModule, FotoModule, HttpModule, PainelModule, routing ],
-    declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
-    bootstrap: [ AppComponent ]
+  imports:      [
+    BrowserModule,
+    HttpModule,
+    PainelModule,
+    FotoModule,
+    routing,
+    FormsModule
+  ],
+  declarations: [ AppComponent, ListagemComponent, CadastroComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
