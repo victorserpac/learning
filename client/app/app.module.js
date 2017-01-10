6ab1e0041b9b13c66a36fe74caf2ed96705a747d
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-10 02:16:02 -0200

    Confirm if it is for delete

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -19,6 +19,7 @@ var foto_module_1 = require("./foto/foto.module");
 var painel_module_1 = require("./painel/painel.module");
 var app_routes_1 = require("./app.routes");
 var forms_1 = require("@angular/forms");
+var botao_module_1 = require("./botao/botao.module");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -33,7 +34,8 @@ AppModule = __decorate([
             foto_module_1.FotoModule,
             app_routes_1.routing,
             forms_1.FormsModule,
-            forms_1.ReactiveFormsModule
+            forms_1.ReactiveFormsModule,
+            botao_module_1.BotaoModule
         ],
         declarations: [app_component_1.AppComponent, listagem_component_1.ListagemComponent, cadastro_component_1.CadastroComponent],
         bootstrap: [app_component_1.AppComponent]
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -10,6 +10,7 @@ import { FotoModule } from './foto/foto.module';
 import { PainelModule } from './painel/painel.module';
 import { routing } from './app.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BotaoModule } from './botao/botao.module';
 
 @NgModule({
   imports:      [
@@ -19,7 +20,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FotoModule,
     routing,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BotaoModule
   ],
   declarations: [ AppComponent, ListagemComponent, CadastroComponent ],
   bootstrap:    [ AppComponent ]
