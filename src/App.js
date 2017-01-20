c95b1463748478baed75a4c518fdba73c56fa506
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-20 20:31:00 -0200

    Refactor the Author registration

--- src/App.js
+++ src/App.js
@@ -1,71 +1,9 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado';
-import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
+import { FormularioAutor,TabelaAutores } from './Autor';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      lista: [],
-      nome: '',
-      email: '',
-      senha: ''
-    };
-
-    this.enviaForm = this.enviaForm.bind( this );
-    this.setNome   = this.setNome.bind( this );
-    this.setEmail  = this.setEmail.bind( this );
-    this.setSenha  = this.setSenha.bind( this );
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',
-      dataType: 'json',
-      success: function( resposta ) {
-        this.setState( { lista: resposta } );
-      }.bind( this )
-    });
-  }
-
-  enviaForm( evento ) {
-    evento.preventDefault();
-
-    $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({
-        nome: this.state.nome,
-        email: this.state.email,
-        senha: this.state.senha
-      }),
-      success: function( resposta ) {
-        console.log( "enviado com sucesso" );
-        this.setState( { lista: resposta } );
-      }.bind(this),
-      error: function( resposta ) {
-        console.log( "erro" );
-      }
-    });
-  }
-
-  setNome( evento ) {
-    this.setState( { nome: evento.target.value } );
-  }
-
-  setEmail( evento ){
-    this.setState( { email: evento.target.value } );
-  }
-
-  setSenha( evento ){
-    this.setState( { senha: evento.target.value } );
-  }
 
   render() {
     return (
@@ -92,38 +30,8 @@ class App extends Component {
             <h1>Cadastro de Autores</h1>
           </div>
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={ this.enviaForm } method="post">
-                <InputCustomizado id="nome" type="text" name="nome" value={ this.state.nome } onChange={ this.setNome } label="Nome"/>
-                <InputCustomizado id="email" type="email" name="email" value={ this.state.email } onChange={ this.setEmail } label="Email"/>
-                <InputCustomizado id="senha" type="password" name="senha" value={ this.state.senha } onChange={ this.setSenha } label="Senha"/>
-
-                <BotaoSubmitCustomizado label="Gravar"/>
-              </form>
-
-            </div>
-            <div>
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.map(function(autor){
-                      return (
-                        <tr key={ autor.id }>
-                          <td>{ autor.nome }</td>
-                          <td>{ autor.email }</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
+            <FormularioAutor/>
+            <TabelaAutores/>
           </div>
         </div>
       </div>
