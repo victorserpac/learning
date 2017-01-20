5c080a04db2c9014ee6435a6d7ef52ddd2e85659
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-20 17:23:25 -0200

    Create custom components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 
 class App extends Component {
 
@@ -92,22 +94,11 @@ class App extends Component {
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
               <form className="pure-form pure-form-aligned" onSubmit={ this.enviaForm } method="post">
-                <div className="pure-control-group">
-                  <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value={ this.state.nome } onChange={ this.setNome }/>
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value={ this.state.email } onChange={ this.setEmail }/>
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" value={this.state.senha} onChange={ this.setSenha }/>
-                </div>
-                <div className="pure-control-group">
-                  <label></label>
-                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                </div>
+                <InputCustomizado id="nome" type="text" name="nome" value={ this.state.nome } onChange={ this.setNome } label="Nome"/>
+                <InputCustomizado id="email" type="email" name="email" value={ this.state.email } onChange={ this.setEmail } label="Email"/>
+                <InputCustomizado id="senha" type="password" name="senha" value={ this.state.senha } onChange={ this.setSenha } label="Senha"/>
+
+                <BotaoSubmitCustomizado label="Gravar"/>
               </form>
 
             </div>
