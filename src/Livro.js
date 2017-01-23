b54b0e02039c214f1fc0f925d1a5278cd8f7f4c7
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-23 19:38:26 -0200

    Create book registration

--- src/Livro.js
+++ src/Livro.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-// import InputCustomizado from './componentes/InputCustomizado';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 import PubSub from 'pubsub-js';
-// import TratadorErros from './TratadorErros';
+import TratadorErros from './TratadorErros';
 
 class TabelaLivros extends Component {
 
@@ -36,12 +37,93 @@ class TabelaLivros extends Component {
   }
 }
 
+class FormularioLivro extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+      titulo: '',
+      preco: '',
+      autorID: ''
+    };
+
+    this.enviaForm  = this.enviaForm.bind( this );
+    this.setTitulo  = this.setTitulo.bind( this );
+    this.setPreco   = this.setPreco.bind( this );
+    this.setAutorId = this.setAutorId.bind( this );
+  }
+
+  enviaForm( evento ) {
+    evento.preventDefault();
+
+    $.ajax({
+      url: 'http://cdc-react.herokuapp.com/api/livros',
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({
+        titulo: this.state.titulo,
+        preco: this.state.preco,
+        autorId: this.state.autorId
+      }),
+      success: function( novaListagem ) {
+        PubSub.publish( 'atualiza-lista-livros', novaListagem );
+        this.setState( { titulo: '', preco: '', autorId: '' } );
+      }.bind( this ),
+      error: function( resposta ) {
+        if( resposta.status === 400 ) {
+          new TratadorErros().publicaErros( resposta.responseJSON );
+        }
+      },
+      beforeSend: function() {
+        PubSub.publish( "limpa-erros", {} );
+      }
+    });
+  }
+
+  setTitulo( evento ) {
+    this.setState( { titulo: evento.target.value } );
+  }
+
+  setPreco( evento ){
+    this.setState( { preco: evento.target.value } );
+  }
+
+  setAutorId( evento ){
+    this.setState( { autorId: evento.target.value } );
+  }
+
+  render() {
+    return(
+      <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={ this.enviaForm } method="post">
+          <InputCustomizado id="titulo" type="text" name="titulo" value={ this.state.titulo } onChange={ this.setTitulo } label="Título"/>
+          <InputCustomizado id="preco" type="text" name="preco" value={ this.state.preco } onChange={ this.setPreco } label="Preço"/>
+            <div className="pure-control-group">
+              <label htmlFor="autorId">Autor</label>
+              <select value={ this.state.autorId } name="autorId" id="autorID" onChange={ this.setAutorId }>
+                  <option value="">Selecione autor</option>
+                    {
+                        this.props.autores.map( function( autor ) {
+                            return <option value={ autor.id } key={ autor.id }>{ autor.nome }</option>
+                        })
+                    }
+              </select>
+            </div>
+
+          <BotaoSubmitCustomizado label="Gravar"/>
+        </form>
+      </div>
+    );
+  }
+}
+
 
 export default class LivroBox extends Component {
 
   constructor() {
     super();
-    this.state = { lista : [] };
+    this.state = { lista : [], autores:[] };
   }
 
   render() {
@@ -51,6 +133,7 @@ export default class LivroBox extends Component {
           <h1>Cadastro de livros</h1>
         </div>
         <div className="content" id="content">
+          <FormularioLivro autores={ this.state.autores }/>
           <TabelaLivros lista={ this.state.lista }/>
         </div>
       </div>
@@ -63,6 +146,14 @@ export default class LivroBox extends Component {
       dataType: 'json',
       success: function( resposta ) {
         this.setState( { lista: resposta } );
+      }.bind( this )
+    });
+
+    $.ajax({
+      url: 'http://cdc-react.herokuapp.com/api/autores',
+      dataType: 'json',
+      success: function( resposta ) {
+        this.setState( { autores: resposta } );
       }.bind( this )
     });
 
