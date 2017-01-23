6e5f353da9c36be1c796a9eda27f3391983cbe01
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-23 18:01:51 -0200

    Implement Browse routes with History API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AutorBox from './Autor';
 import './index.css';
 import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render(
   (<Router history={ browserHistory }>
-    <Route path="/" component={App}/>
-    <Route path="/autor"/>
+    <Route path="/" component={ App }/>
+    <Route path="/autor" component={ AutorBox }/>
     <Route path="/livro"/>
   </Router>),
   document.getElementById('root')
