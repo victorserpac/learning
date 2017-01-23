df6a1c057eb25c54e72a6b71805c653f674b8012
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-23 17:56:14 -0200

    React Router configuration

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render(
-  <App />,
+  (<Router history={ browserHistory }>
+    <Route path="/" component={App}/>
+    <Route path="/autor"/>
+    <Route path="/livro"/>
+  </Router>),
   document.getElementById('root')
 );
