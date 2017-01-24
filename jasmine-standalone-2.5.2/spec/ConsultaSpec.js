b0d7ceb7063ea21d70f29e536efd534a72ad570e
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-24 17:14:30 -0200

    Improve the tests code

--- jasmine-standalone-2.5.2/spec/ConsultaSpec.js
+++ jasmine-standalone-2.5.2/spec/ConsultaSpec.js
@@ -1,31 +1,48 @@
 describe( 'Consulta', function() {
+  var guilherme;
 
-  it( 'nao deve cobrar nada se a consulta for um retorno', function() {
-    var guilherme = new Paciente( 'Guilherme', 28, 72, 1.80 );
-    var consulta = new Consulta( guilherme, [], true, true );
-
-    expect( consulta.preco() ).toEqual( 0 );
+  beforeEach( function() {
+    guilherme = new PacienteBuilder().constroi();
   });
 
-  it( 'deve cobrar 25 por cada procedimento comum', function() {
-    var guilherme = new Paciente( 'Guilherme', 28, 72, 1.80 );
-    var consulta = new Consulta( guilherme, [ 'proc1', 'proc2' ], false, false );
+  describe( 'Consultas que são retornos', function() {
 
-    expect( consulta.preco() ).toEqual( 50 );
+    it( 'nao deve cobrar nada se a consulta for um retorno', function() {
+      var consulta = new Consulta( guilherme, [], true, true );
+
+      expect( consulta.preco() ).toEqual( 0 );
+    });
+
   });
 
-  it( 'deve dobrar o preco da consulta particular', function() {
-    var guilherme = new Paciente( 'Guilherme', 28, 72, 1.80 );
-    var consulta = new Consulta( guilherme, [ 'proc1', 'proc2' ], true, false );
+  describe( 'Consultas que são particulares', function() {
 
-    expect( consulta.preco() ).toEqual( 50 * 2 );
+    it( 'deve dobrar o preco da consulta particular', function() {
+      var consulta = new Consulta( guilherme, [ 'proc1', 'proc2' ], true, false );
+
+      expect( consulta.preco() ).toEqual( 50 * 2 );
+    });
+
+    it( 'deve dobrar o preco da consulta particular mesmo com procedimentos especiais', function() {
+      var consulta = new Consulta( guilherme, [ 'raio-x' ], true, false );
+
+      expect( consulta.preco() ).toEqual( 55 * 2 );
+    });
   });
 
-  it( 'deve cobrar preco especifico dependendo do procedimento', function() {
-    var guilherme = new Paciente( 'Guilherme', 28, 72, 1.80 );
-    var consulta = new Consulta( guilherme, [ 'procedimento-comum', 'raio-x', 'procedimento-comum2', 'gesso' ], false, false );
+  describe( 'Consultas por um convênio', function() {
 
-    expect( consulta.preco() ).toEqual( 25 + 55 + 25 + 32 );
+    it( 'deve cobrar 25 por cada procedimento comum', function() {
+      var consulta = new Consulta( guilherme, [ 'proc1', 'proc2' ], false, false );
+
+      expect( consulta.preco() ).toEqual( 50 );
+    });
+
+    it( 'deve cobrar preco especifico dependendo do procedimento', function() {
+      var consulta = new Consulta( guilherme, [ 'procedimento-comum', 'raio-x', 'procedimento-comum2', 'gesso' ], false, false );
+
+      expect( consulta.preco() ).toEqual( 25 + 55 + 25 + 32 );
+    });
   });
 
 });
