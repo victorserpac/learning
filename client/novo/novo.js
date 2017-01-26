e82ef848ccc365a76230c1ae59d2a366112097e8
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-26 00:14:08 -0200

    Implement the task creation form

---
+++ client/novo/novo.js
@@ -0,0 +1,11 @@
+Template.novo.events({
+  'submit form': function( e, template ) {
+    e.preventDefault();
+
+    var input = $( '#tarefa' );
+    var nome = input.val();
+
+    Tarefas.insert( { nome: nome, data: new Date() } );
+    input.val( '' );
+  }
+});
