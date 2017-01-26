1f6300f8ae15d5259c8ecaac5a20ca3aa61d7397
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-26 00:01:25 -0200

    Install bootstrap to improve the interface

--- client/lista/lista.js
+++ client/lista/lista.js
@@ -1,5 +1,9 @@
 Template.lista.helpers({
   tarefas: function() {
     return Tarefas.find({});
+  },
+
+  formataData: function() {
+    return moment( this.data ).format( 'DD/MM/YYYY HH:mm' );
   }
 });
