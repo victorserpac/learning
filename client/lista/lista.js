9c08c7c218fa297db92facf05e1ca840cd5b1f2f
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-26 00:18:11 -0200

    Implement the remove button actions

--- client/lista/lista.js
+++ client/lista/lista.js
@@ -7,3 +7,11 @@ Template.lista.helpers({
     return moment( this.data ).format( 'DD/MM/YYYY HH:mm' );
   }
 });
+
+Template.lista.events({
+  'click button': function( e, template ) {
+    var tarefa = this;
+
+    Tarefas.remove({ '_id': tarefa._id });
+  }
+});
