db6b9e0fb820764e377efa8a9f69a0766d72b1c9
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-25 22:36:22 -0200

    Create first list with static data

---
+++ client/lista/lista.js
@@ -0,0 +1,9 @@
+Template.lista.helpers({
+  tarefas: function() {
+    return [
+      { nome: 'Ir ao mercado' },
+      { nome: 'Estudar javascript' },
+      { nome: 'Comprar ingresso pro show do Maroon 5' }
+    ];
+  }
+});
