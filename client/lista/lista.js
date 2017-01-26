66516e45ca6e3d76929a53ef7ae8fe81c4e0167f
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-25 22:36:56 -0200

    Get data from MongoDB

--- client/lista/lista.js
+++ client/lista/lista.js
@@ -1,9 +1,5 @@
 Template.lista.helpers({
   tarefas: function() {
-    return [
-      { nome: 'Ir ao mercado' },
-      { nome: 'Estudar javascript' },
-      { nome: 'Comprar ingresso pro show do Maroon 5' }
-    ];
+    return Tarefas.find({});
   }
 });
